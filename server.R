b27cea7d464ce5a72756cea5c9a1b4cc490cbd3b
H Atib <bisima@gmail.com>
2017-01-26 20:24:06 +1100

    Assignment Four

--- server.R
+++ server.R
@@ -1,10 +1,5 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+# This is the server logic of a Shiny Fuel web application.
 #
 
 library(shiny)
